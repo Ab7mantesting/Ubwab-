4e5421db3f4df8a4ac4d7f9ea03bc0ae69a6f583
Ab7mantesting <ab7man21@hotmail.com>
2023-11-05 01:43:11 +0300

    Initial Commit

---
+++ Ubwab/UbwabApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbwabApp.swift
+//  Ubwab
+//
+//  Created by A.rahman Alrumaihi on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct UbwabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
